f449301730f530002e5da9e5c980909cfdc47b6d
Bob Larrick <larrick@gmail.com>
2011-05-13 17:37:34 -0400

    Driver block one is good

--- driver.rb
+++ driver.rb
@@ -13,6 +13,8 @@ end
 # @s.address = @s.external_ip
 # puts "lat_long", @s.lat_long
 
+
+puts "Driver Block One ----------------------"
 printer "  current_julian_cycle "
 printer "   current_julian_date "
 printer "approximate_solar_noon "
@@ -22,30 +24,20 @@ printer "    ecliptic_longitude "
 printer "       right_ascension "
 printer "declination_of_the_sun "
 puts
-printer " l_sun "
-printer " jpp "
-printer "sunset_jd_two "
-puts "XXXXXXXXXXXXXXXX"
 
-printer "sidereal_time "
+puts "Driver Block Two ----------------------"
+printer "         l_sun "
+printer "           jpp "
+printer " sunset_jd_two "
 puts
-#printer "hour_angle_two "
-printer "hour_angle "
-printer "solar_transit "
 
-#printer "hour_angle "
-
-printer 'sunrise_jd '
-# JD 2450499.236300 is
-# CE 1997 February 19 17:40:16.3 UT  Wednesday
-#JD 2455694.789580 is
-#CE 2011 May 13 06:56:59.7 UT  Friday
-
-printer " sunset_jd "
-#JD 2455694.786370 is
-#CE 2011 May 13 06:52:22.4 UT  Friday
-# JD 2455694.791220 is
-# CE 2011 May 13 06:59:21.4 UT  Friday
-printer " sunrise "
-printer "  sunset "
+puts "Driver Block Three ----------------------"
+printer " sidereal_time "
+printer "    hour_angle "
+printer " solar_transit "
+printer '    sunrise_jd '
+printer "     sunset_jd "
+puts
+printer "   sunrise "
+printer "    sunset "
 
